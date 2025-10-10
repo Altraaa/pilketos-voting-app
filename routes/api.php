38c6ae1f5e9be888a16a4712c494224684d73f1c
Altraaa <aldimks7464@gmail.com>
2025-10-10 23:43:19 +0800

    Feat: Create Endpoint

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\VoteController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // User Routes
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+    // Candidate Routes
+    Route::get('/candidates', [CandidateController::class, 'index']);
+    Route::post('/candidates', [CandidateController::class, 'store']);
+    Route::get('/candidates/{id}', [CandidateController::class, 'show']);
+    Route::put('/candidates/{id}', [CandidateController::class, 'update']);
+    Route::delete('/candidates/{id}', [CandidateController::class, 'destroy']);
+    
+    // Vote Routes
+    Route::get('/votes', [VoteController::class, 'index']);
+    Route::post('/votes', [VoteController::class, 'store']);
+    Route::get('/candidates/{candidateId}/votes', [VoteController::class, 'showByCandidate']);
+});
