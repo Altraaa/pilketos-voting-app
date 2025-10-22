32599edecf247ff6efd5462692b752a4b3484d6f
Altraaa <aldimks7464@gmail.com>
2025-10-22 19:01:03 +0800

    Feat: Create Admin Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'admin' => \App\Http\Middleware\AdminMiddleware::class, // Tambahkan ini
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
